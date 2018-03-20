ec313a82062849fd4c6fe62672b2fa6974090c0b
sysreqx <sysreqx@gmail.com>
2018-03-20 22:24:05 +0600

    yahz

---
+++ itstep/acmp.ru/acmp0312.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+	int a, b, c, d, e;
+	cin >> a >> b >> c;
+
+	d = b - a;
+	e = (c - 1) * d + a;
+	cout << e << endl;
+	
+	// system("pause");
+	return 0;
+}
---
+++ itstep/acmp.ru/acmp0331.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{   
+	char ch;
+	int h, m;
+	int h_s, m_s;
+	cin >> h >> ch >> m;
+	cin >> h_s >> m_s;
+
+	h = (h + h_s) % 24 + (m + m_s) / 60;
+	m = (m + m_s) % 60;
+
+	cout << setfill('0') << setw(2) << h << ch << setfill('0') << setw(2) << m << endl;
+	
+	// system("pause");
+	return 0;
+}
