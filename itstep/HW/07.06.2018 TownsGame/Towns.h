fd8980087cddc87cab9a880b87b419834473750c
sysreqx <sysreqx@gmail.com>
2018-06-16 13:39:26 +0600

    Towns Game

---
+++ itstep/HW/07.06.2018 TownsGame/Towns.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <map>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <Windows.h>
+#include <vector>
+using namespace std;
+
+class townsGame {
+	map < string, int > towns;
+	char nextWord = '.';
+	string dir;
+	vector<int> maxPlayers;
+public:
+	townsGame(string dir, int mP);
+	void play();
+	void check_city(string city) const;
+	void multiplay(int pos);
+};
+
+
---
+++ itstep/HW/07.06.2018 TownsGame/main.cpp
@@ -0,0 +1,14 @@
+﻿#include "Towns.h"
+
+
+int main(int argc,char*argv[]) {
+	
+	setlocale(0, "");
+	system("chcp 1251");
+	system("cls");
+	// \\dc\Студенты\ПКО\SMB - 172.2\city.csv
+	townsGame t("C:\\Users\\SysRq\\source\\repos\\Project1\\Project1", atoi(argv[2]));
+	t.multiplay(atoi(argv[1]));
+	//t.play();
+	return 0;
+}
